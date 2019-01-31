814ebe8a874e9e9c5c561b8bc51cdcb1b7021afb
Porter Savage and Tanner Damron <tanner@>
2019-01-31 11:36:14 -0800

    refactor more code and add styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 // Business Logic -----
-function Player() {
+function Player(name) {
+    this.name = name,
     this.points = 0,
     this.roundPoints = 0,
     this.currentNumber = 0
@@ -24,44 +25,55 @@ Player.prototype.addToPoints = function() {
     this.points += this.roundPoints;
     $("#playerTwo").toggle();
     $("#playerOne").toggle();
+    this.roundPoints = 0
 }
 
 var diceRoll = function () {
     return Math.floor(Math.random() * 6) + 1;   
 };
 
+Player.prototype.checkForWin = function() {
+    if (this.points >= 100) {
+        alert("You've reached 100 points " + this.name + ", YOU WON!!")
+    }
+}
 
 // UI Logic -----
 
 $(document).ready(function() {
-    $("form#playerOneForm").submit(function(event) {
+    $("#playerOneDice").click(function(event) {
         event.preventDefault();
+        $(this).addClass('magictime twisterInDown');
         var num = diceRoll();
         player1.pointLogic(num);
         $(".rollOutput").text(player1.currentNumber);
-        $(".roundPointOutput").text(player1.roundPoints)
-        console.log(player1.roundPoints);
+        $(".roundPointOutput").text(player1.roundPoints);
+        $(".playerOneTotal").text(player1.points);
+        player1.checkForWin();
     });
 
     $("#playerOneHold").click(function(event) {
         event.preventDefault();
         player1.addToPoints();
-        console.log(player1.points);
+        $(".playerOneTotal").text(player1.points);
+        player1.checkForWin();
     });
 
-    $("form#playerTwoForm").submit(function(event) {
+    $("#playerTwoDice").click(function(event) {
         event.preventDefault();
+        $(this).addClass('magictime twisterInDown');
         var num = diceRoll();
         player2.pointLogic(num);
         $(".rollOutput").text(player2.currentNumber);
         $(".roundPointOutput").text(player2.roundPoints);
-        console.log(player2.roundPoints);
+        $(".playerTwoTotal").text(player2.points);
+        player2.checkForWin();
     });
 
     $("#playerTwoHold").click(function(event) {
         event.preventDefault();
         player2.addToPoints();
-        console.log(player2.points);
+        $(".playerTwoTotal").text(player2.points);
+        player2.checkForWin();
     });
-
 });
